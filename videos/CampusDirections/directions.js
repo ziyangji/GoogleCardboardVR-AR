4c14ecf7d3c2bd9f2a10333210008cc41d74b538
shanalily <shanalily@gmail.com>
2017-11-04 15:48:18 -0400

    added options, button is working, started find images function

--- videos/CampusDirections/directions.js
+++ videos/CampusDirections/directions.js
@@ -1,3 +1,5 @@
+var locations = [];
+
 // $("#mybody").hide();
 $.ajax({
 	type: "GET",
@@ -5,7 +7,12 @@ $.ajax({
 	dataType: "json",
 	success: function(data){
 		mybody = mySite(data);
-		// console.log();
+		// added by Shoshana, didn't see mySite function when I wrote this
+		var start = document.getElementById("start");
+		for (var i = 0; i < data.buildings.length; ++i) {
+			console.log(data.buildings[i].building);
+			start += "<option value=" + data.buildings[i].address + ">" + data.buildings[i].building + "</option>";
+		}
 	}, 
 	error: function(msg){
 		// console.log();
@@ -28,5 +35,18 @@ function mySite(data){
 		option2.text = building;
 		select1.add(option1, i+1);
 		select2.add(option2, i+1);
+	}
+}
+
+function findPath(locations, path) {
+	// think about making an efficient data structure?
+	var images = []
+	for (var i = 0; i < path.length; ++i) {
+		// find the closest location
+		for (var j = 0; j < locations.length; ++j) {
+			// if the difference between x and y on path and x and y at location are small enough,
+			// add to array
+			// might need to check there aren't duplicates (not sure to do it here or at end)
+		}
 	}
 }
