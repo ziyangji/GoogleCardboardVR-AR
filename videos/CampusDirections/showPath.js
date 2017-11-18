1e8a7a4512681e45582cf19c1b14ab232619f190
shanalily <shanalily@gmail.com>
2017-11-18 13:44:50 -0500

    can click through path now

--- videos/CampusDirections/showPath.js
+++ videos/CampusDirections/showPath.js
@@ -2,12 +2,15 @@
 // Can we link the directions.html file to this file
 // By doing this, I mean to have the directions.html file write to a json file 
 // we can get the data from the json file instead of doing this
-var imagePath = ["../../pictures/nearempacequirectangular.jpg", "../../pictures/86field.jpg", "../../pictures/quadequirectangular.jpg"];
+var imagePath = ["../../pictures/nearempacequirectangular.jpg", "../../pictures/86field.jpg", "../../pictures/quadequirectangular.jpg", "../../pictures/empacequirectangular.jpg"];
 
 var locations = [];
 
 window.onload = function() {
+	var counter = 1;
+
 	pushToAssets();
+	addButton();
 
 	function pushToAssets() {
 		var assets = document.querySelector('a-assets');
@@ -21,12 +24,63 @@ window.onload = function() {
 		}
 
 		var sky = document.createElement('a-sky');
+		sky.setAttribute('id', 'loc');
 		sky.setAttribute('src', '#image0');
+		sky.setAttribute('data-set-image-fade-setup', 'true');
 		var world = document.querySelector('a-scene');
 		world.appendChild(sky);
-
-		// test
-		var test = document.querySelector('a-assets');
-		console.log(test.querySelector('#image0'));
 	}
+
+	function addButton() {
+		// var link = document.getElementById('links');
+		var link = document.querySelector('a-entity');
+		var button = document.createElement('a-entity');
+		button.setAttribute('template', 'src: #button');
+		button.setAttribute('data-src', imagePath[1]);
+		button.setAttribute('data-thumb', 'arrow.png'); // maybe I need to make a 3d primitive arrow
+		link.appendChild(button);
+	}
+
+	// based on aframe tutorial
+	AFRAME.registerComponent('set-image', {
+				schema: {
+					on: {type: 'string'},
+					target: {type: 'selector'},
+					src: {type: 'string'},
+					dur: {type: 'number', default: 300}
+				},
+				init: function() {
+					var data = this.data;
+					var el = this.el;
+					this.setupFadeAnimation();
+					el.addEventListener(data.on, function() {
+						data.target.emit('set-image-fade');
+						setTimeout(function() {
+							data.target.setAttribute('material', 'src', data.src);
+							//
+							// var button = document.querySelector('a-entity').querySelector('a-entity');
+							// button.setAttribute('data-src', imagePath[++counter]);
+							data.src = imagePath[++counter];
+						}, data.dur);
+					});
+				},
+				setupFadeAnimation: function() {
+					var data = this.data;
+				    var targetEl = this.data.target;
+
+				    // Only set up once.
+				    if (targetEl.dataset.setImageFadeSetup) { return; }
+				    targetEl.dataset.setImageFadeSetup = true;
+
+				    // Create animation.
+				    targetEl.setAttribute('animation__fade', {
+				      property: 'material.color',
+				      startEvents: 'set-image-fade',
+				      dir: 'alternate',
+				      dur: data.dur,
+				      from: '#FFF',
+				      to: '#000'
+				    });
+				}
+			});
 }
